0f8213330c92e1bda6ee6bc2446e43864a080d6c
Sunandan Bose <sunandan.riders@gmail.com>
2021-12-12 23:57:42 +0530

    load data for any given category

--- backend/src/main/java/com/project/stocks/controller/CategoryController.java
+++ backend/src/main/java/com/project/stocks/controller/CategoryController.java
@@ -3,10 +3,7 @@ package com.project.stocks.controller;
 import com.project.stocks.dto.Category;
 import com.project.stocks.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,4 +24,8 @@ public class CategoryController {
         return categoryService.getCategoryNames();
     }
 
+    @PostMapping(value = "/{category}")
+    public void loadCompanyData(@PathVariable(value = "category") String category){
+        categoryService.loadCompanyData(category);
+    }
 }
--- backend/src/main/java/com/project/stocks/service/CategoryService.java
+++ backend/src/main/java/com/project/stocks/service/CategoryService.java
@@ -12,16 +12,32 @@ public class CategoryService {
 
     private CategoryRepository categoryRepository;
 
+    private ScrapingService scrapingService;
+
     @Autowired
-    public CategoryService(CategoryRepository categoryRepository) {
+    public CategoryService(CategoryRepository categoryRepository, ScrapingService scrapingService) {
         this.categoryRepository = categoryRepository;
+        this.scrapingService = scrapingService;
     }
 
-    public Category getCategoryDetails(String category){
+    public Category getCategoryDetails(String category) {
         return categoryRepository.getCategoryDetails(category);
     }
 
     public List<String> getCategoryNames() {
         return categoryRepository.getCategoryNames();
+    }
+
+    public void loadCompanyData(String category) {
+        Category data = getCategoryDetails(category);
+        List<String> companyList = data.getCompanyList();
+        for (int i = 0; i < companyList.size(); i++) {
+            try {
+                scrapingService.add(companyList.get(i));
+            } catch (Exception e) {
+                System.out.println("Couldn't load data for stock : " + companyList.get(i) +
+                        " Error : " + e.getMessage());
+            }
+        }
     }
 }
