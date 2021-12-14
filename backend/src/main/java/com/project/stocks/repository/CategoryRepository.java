b47966cd8cba5ccaf4d3fc16b278fdcdf7d9b861
Sunandan Bose <sunandan.riders@gmail.com>
2021-12-14 21:31:08 +0530

    Seperate AWS code outside

--- backend/src/main/java/com/project/stocks/repository/CategoryRepository.java
+++ backend/src/main/java/com/project/stocks/repository/CategoryRepository.java
@@ -1,69 +1,45 @@
 package com.project.stocks.repository;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.project.stocks.dto.Category;
 import com.project.stocks.service.DataMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 @Component
 public class CategoryRepository {
 
+    private final String bucketName = "stock-ui-bucket";
+
+    private S3AWSRepository s3AWSRepository;
+
+    @Autowired
+    public CategoryRepository(S3AWSRepository s3AWSRepository) {
+        this.s3AWSRepository = s3AWSRepository;
+    }
+
     public Category getCategoryDetails(String categoryName) {
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
         Category category = new Category();
         try {
             String key_name = "category/" + categoryName;
-            S3Object o = s3.getObject("stock-ui-bucket", key_name);
-            S3ObjectInputStream s3is = o.getObjectContent();
-            Scanner s = new Scanner(s3is);
-            StringBuilder sb = new StringBuilder();
-            while(s.hasNext()){
-                sb.append(s.next());
-            }
-            String result = sb.toString();
+            String result = s3AWSRepository.getData(bucketName, key_name);
             category = DataMapper.mapCategory(result);
-            s3is.close();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
             e.printStackTrace();
         }
         return category;
     }
 
     public List<String> getCategoryNames() {
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
         List<String> categoryNames = new ArrayList<>();
         try {
             String key_name = "categoryList";
-            S3Object o = s3.getObject("stock-ui-bucket", key_name);
-            S3ObjectInputStream s3is = o.getObjectContent();
-            Scanner s = new Scanner(s3is);
-            StringBuilder sb = new StringBuilder();
-            while(s.hasNext()){
-                sb.append(s.next());
-            }
-            String result = sb.toString();
+            String result = s3AWSRepository.getData(bucketName, key_name);
             categoryNames.addAll(DataMapper.mapCategoryList(result));
-            s3is.close();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
             e.printStackTrace();
         }
         return categoryNames;
