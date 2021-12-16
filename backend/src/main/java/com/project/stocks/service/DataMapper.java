b125167d02fd4dce94a19b8278001844dca1bd49
Sunandan Bose <sunandan.riders@gmail.com>
2021-12-16 15:32:15 +0530

    Extract method to shift redundant code

--- backend/src/main/java/com/project/stocks/service/DataMapper.java
+++ backend/src/main/java/com/project/stocks/service/DataMapper.java
@@ -13,26 +13,26 @@ public class DataMapper {
 
     public static Stock mapStock(String result) throws JsonProcessingException {
         if(result == null || result.isEmpty()) return null;
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new Jdk8Module());
-        Stock stock = mapper.readValue(result, Stock.class);
+        Stock stock = getObjectMapper().readValue(result, Stock.class);
         return stock;
     }
 
     public static Category mapCategory(String result) throws JsonProcessingException {
         if(result == null || result.isEmpty()) return null;
+        Category category = getObjectMapper().readValue(result, Category.class);
+        return category;
+    }
+
+    private static ObjectMapper getObjectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new Jdk8Module());
-        Category category = mapper.readValue(result, Category.class);
-        return category;
+        return mapper;
     }
 
     public static List<String> mapCategoryList(String result) throws JsonProcessingException {
         if(result == null || result.isEmpty()) return null;
         List<String> categoryNames = new ArrayList<>();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new Jdk8Module());
-        categoryNames.addAll(mapper.readValue(result, List.class));
+        categoryNames.addAll(getObjectMapper().readValue(result, List.class));
         return categoryNames;
     }
 
