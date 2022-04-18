5edd1b14f3490d74a1557a36dcb31d8b435686a1
Tarique <tariqueejaz.93102@gmail.com>
2022-04-18 23:06:28 +0200

    chore: enable localhost for dev env

--- frontend/src/services/ScoreService.ts
+++ frontend/src/services/ScoreService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const getAllStockScores = () => {
-    let endpoint = 'http://localhost:8081/stock/scores';
+    let endpoint = `${process.env.REACT_APP_BACKEND_URL}/stock/scores`;
     return axios.get(endpoint);    
 }
