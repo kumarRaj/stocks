0984a84d12a00a06c901571d0c0e049bf3093497
Tarique Ejaz <tariqueejaz.93102@gmail.com>
2022-04-19 20:36:44 +0700

    [Frontend] - Styling Stock View (#54)
    * feat: toolbar styling
    
    * feat: align stock items
    
    * feat: add changes on breakdown
    
    * feat: add breakdown view and toggle
    
    * chore: no stocks message
    
    * Update yarn.lock
    
    * Remvove unused sass and node-sass
    
    * Reorder UI. Specify details id for stock
    
    * Update README.md
    
    Co-authored-by: Raj <raj.kumar@glovoapp.com>

--- frontend/src/pages/StocksList/StocksList.tsx
+++ frontend/src/pages/StocksList/StocksList.tsx
@@ -2,10 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { getAllSectors, getStocksBySector } from '../../services/SectorService';
 import { getAllStockScores } from '../../services/ScoreService';
 
+import { Grid, MenuItem, Select, TextField, Paper } from '@mui/material';
+import './StocksList.css';
+
 export const StocksList: React.FC = () => {
     let [sectors, setSectors] = useState([]);
     let [stockScores, setStockScores] = useState([]);
     let [currentStocks, setCurrentStocks] = useState([]);
+    let [currentBreakdown, setCurrentBreakdown] = useState<any>({});
+    let [selectedStock, setSelectedStock] = useState('');
+    let [isBreakdownVisible, toggleBreakdownVisible] = useState(false);
     let [currentSector, setCurrentSector] = useState('NIFTY50');
 
     // on component mount
@@ -36,43 +42,77 @@ export const StocksList: React.FC = () => {
         return stockScores.filter((stock: any) => stocksBySector.includes(stock.stockId));
     }
 
+    const handleSectorChange = (event: any) => {
+        setCurrentSector(event.target.value);
+    }
+
+    const handleBreakdownToggle = (breakdown: any, selectedStockId: string) => {
+        toggleBreakdownVisible(true);
+        setCurrentBreakdown(breakdown);
+        setSelectedStock(selectedStockId);
+    }
+
     return (
-        <div>
-            <div className='sectors-main'>
-                {
-                    sectors.map(sector => {
-                        return (
-                            <div onClick={() => setCurrentSector(sector)} key={sector} className='sector-item'>
-                                {sector}
-                            </div>
-                        )
-                    })
-                }
-            </div>
+        <Grid container>
+            <Grid item xs={12} md={12}>
+                <div className='sector-container'>
+                    <div className='sector-selector-main'>
+                        <p>Sectors:</p>
+                        <Select
+                            key={'sector-selector'}
+                            defaultValue={currentSector ?? ' '}
+                            onChange={handleSectorChange}
+                            className='sector-selector'
+                        >
+                            {
+                                sectors.map(sector => {
+                                    return (
+                                        <MenuItem value={sector}>
+                                            {sector}
+                                        </MenuItem>
+                                    )
+                                })
+                            }
+                        </Select>
+                    </div>
 
-            <div>
-                {
-                    currentStocks.map((currentStock: any) => {
-                        let {stockId, scoreBreakdown, score} = currentStock;
-                        return (
-                            <div>
-                                <p>{stockId}</p>
-                                <p>{score}</p>
+                    <TextField className='stock-search' label="Search Stock" variant="outlined" disabled />
+                </div>
+            </Grid>
 
-                                <div>
-                                    <p>Breakdown:</p>
-                                    <p>Borrowings: {scoreBreakdown.borrowings}</p>
-                                    <p>Liabilities: {scoreBreakdown.liabilities}</p>
-                                    <p>Net Profit Margin: {scoreBreakdown.netProfitMargin}</p>
-                                    <p>Operating Profit Margin: {scoreBreakdown.operatingProfitMargin}</p>
-                                    <p>PE Ratio: {scoreBreakdown.pe}</p>
-                                    <p>Revenue: {scoreBreakdown.revenue}</p>
-                                </div>
-                            </div>
-                        );
-                    })
-                }
-            </div>
-        </div>
+            <Grid item xs={12} md={12}>
+                <div className='stocks-container'>
+                    {currentStocks.length === 0 
+                        ? <p>No stocks in this category</p>
+                        : currentStocks.map((currentStock: any) => {
+                            let { stockId, scoreBreakdown, score } = currentStock;
+                            return (
+                                <Paper className='stock-item'>
+                                    <div onClick={() => handleBreakdownToggle(scoreBreakdown, stockId)}>
+                                        <p className='stock-caption'>{stockId}</p>
+                                        <p>{score}</p>
+                                    </div>
+                                </Paper>
+                            );
+                        })
+                    }
+                </div>
+
+                {isBreakdownVisible && <Paper className='stock-breakdown'>
+                    <div>
+                        <p>Breakdown for : {selectedStock}</p>
+                        <p>PE Ratio: {currentBreakdown.pe}</p>
+                        <p>Operating Profit Margin: {currentBreakdown.operatingProfitMargin}</p>
+                        <p>Net Profit Margin: {currentBreakdown.netProfitMargin}</p>
+                        <p>Borrowings: {currentBreakdown.borrowings}</p>
+                        <p>Liabilities: {currentBreakdown.liabilities}</p>
+                        <p>Revenue: {currentBreakdown.revenue}</p>
+
+                        <button onClick={() => toggleBreakdownVisible(false)}>Close</button>
+                    </div>
+                </Paper>}
+            </Grid>
+        </Grid >
+
     );
 }
