22860ecb31313f36eb60c5a9414091387f60ff35
Raj Kumar <raj.kumar@glovoapp.com>
2022-04-23 17:31:18 +0200

    Load pe and sector infomation

--- scraper/server.js
+++ scraper/server.js
@@ -3,6 +3,7 @@ var app = express();
 const scraper = require("./index")
 const categories = require("./category/loadNSECategories")
 const peerInfo = require("./peerInfo");
+const peInfo = require("./peInfo");
 
 app.get('/ping', function (req, res) {
     res.end( "Successfull!!!" );
@@ -28,8 +29,8 @@ app.post('/stockDetails', async function (req, res) {
 })
 
 
-app.post('/peers', async function (req, res) {
-    var stockId = req.query.stockId;
+app.post('/stock/peers', async function (req, res) {
+    const stockId = req.query.stockId;
     try{
         if (stockId){
             await peerInfo.fetchPeers(stockId);
@@ -46,11 +47,31 @@ app.post('/peers', async function (req, res) {
     }
 })
 
+
+app.post('/stock/pe', async function (req, res) {
+    const stockId = req.query.stockId;
+    try{
+        if (stockId){
+            await peInfo.fetchPE(stockId);
+        }
+        res.end()
+    } catch (error) {
+        res.status(error.status || 500);
+        res.json({
+            error: {
+                stockId : stockId,
+                message: error.message,
+            },
+        });
+    }
+})
+
+
 //Loads company names in specific category, Also stores the category names
 categories.fetchCategories()
 
-var server = app.listen(9000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("scraper app listening at http://localhost:%s", port)
-})
+const server = app.listen(9000, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("scraper app listening at http://localhost:%s", port)
+});
