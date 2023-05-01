8dea531b46e624b3406d8a7a003425c73fa2329f
Raj Kumar <raj.kumar@glovoapp.com>
2023-05-01 21:04:13 +0200

    Use cookies to get data out of nse

--- scraper/category/loadNSECategories.js
+++ scraper/category/loadNSECategories.js
@@ -1,18 +1,32 @@
 const axios = require('axios');
 const resources = require('../constants/resources')
-const saveLocal = require("../saveLocal")
+const fileSystem = require("../fileSystem")
 
 
 function getCompanyNames(url) {
-    return axios.get(url)
-        .then(function (response) {
-            companies = response.data.data;
-            companyNames = companies.map( company => company.symbol)
-            return companyNames
-        })
-        .catch(function (error) {
-            console.log("Error fetching companies for: " + url);
-        })
+    axios.get('https://www.nseindia.com/')
+        .then((response) => {
+            const responseHeaders = response.headers
+            console.log(responseHeaders['set-cookie'])
+            const options = {
+                headers: {
+                    'Cookie': responseHeaders['set-cookie']
+                }
+            }
+            axios.get(url, options)
+                .then(function (response) {
+                    companies = response.data.data;
+                    companyNames = companies.map(company => company.symbol)
+                    return companyNames
+                })
+                .then(function (companyNames) {
+                    console.log(companyNames)
+                    // return companyNames
+                })
+                .catch(function (error) {
+                    console.log("Error fetching companies for: " + url + " " + error);
+                });
+        });
 }
 
 async function fetchCategories(){
@@ -28,10 +42,14 @@ async function fetchCategories(){
 async function saveCategoryNames() {
     let categories = resources.NSECategories;
     let names = []
-    for(let i = 0; i < categories.length; i++){
+    for (let i = 0; i < categories.length; i++) {
         names[i] = categories[i].name
     }
-    await saveLocal.save(names, "category", "CategoryNames")
+    // await fileSystem.save(names, "category", "CategoryNames")
 }
 
 module.exports = {fetchCategories};
+
+// fetchCategories()
+
+getCompanyNames("https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%20AUTO")
